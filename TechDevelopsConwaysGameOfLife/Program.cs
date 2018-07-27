c5c8562da9aa6dc6ce049e7b5326a1291564a2e5
Sarah Wood <sarah.wood@asos.com>
2018-07-27 11:53:27 +0100

    Classes have been defined

--- TechDevelopsConwaysGameOfLife/Program.cs
+++ TechDevelopsConwaysGameOfLife/Program.cs
@@ -6,10 +6,60 @@ using System.Threading.Tasks;
 
 namespace TechDevelopsConwaysGameOfLife
 {
+    class Game
+    {
+        
+        public Game()
+        {
+            var grid = new Grid();
+        }
+    }
+
+    class Grid
+    {
+        private readonly int height = 2;
+        private readonly int width = 2;
+
+        public Grid()
+        {
+            var seed = new Seed(height, width);
+        }
+    }
+
+    class Seed
+    {
+        public Seed(int height, int width)
+        {
+            
+        }
+    }
+
+    class Cell
+    {
+    }
+
+    class Alive : Cell
+    {
+    }
+
+    class Dead : Cell
+    {
+    }
+
+    class DeadNeighbour : Dead
+    {
+    }
+
+    class AliveNeighbour : Alive
+    {
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
